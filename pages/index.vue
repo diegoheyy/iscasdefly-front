96bfabac4df4225c8397c852a0c1884a73475761
Diego Hey <diegoheyy@gmail.com>
2021-04-28 10:59:42 -0300

    inicial

---
+++ pages/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="text-h2 primary--text">
+        <span class="text-h4 d-none d-sm-inline">www.</span>Iscas de Fly<span
+          class="text-h4 d-none d-sm-inline">.com.br</span
+        >
+      </h1>
+      <div class="text-body-2 subtitle mt-5">
+        Em breve um Blog sobre o mundo do Fly Fishing e as maravilhas da pesca.
+      </div>
+      <div class="links">
+        <!-- <v-btn color="primary" outlined small nuxt href="/post"> Blog </v-btn> -->
+        <v-btn
+          color="primary"
+          outlined
+          small
+          target="_blank"
+          href="https://www.instagram.com/explore/tags/pescacommosca/"
+        >
+          Instagram <v-icon>mdi-instagram</v-icon>
+        </v-btn>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 90vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 250px !important;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
---
+++ pages/post/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <vcontainer>
+    <v-btn color="primary" elevation="2" large>ALOW</v-btn>
+    <v-container class="pa-1">
+      <v-item-group>
+        <v-row>
+          <v-col v-for="(post, i) in posts" :key="i" cols="12" md="6">
+            <v-img :src="`${post.fimg_url}`"  height="150" ></v-img>
+            <v-item >
+                     {{post.title.rendered}}
+            </v-item>
+          </v-col>
+        </v-row>
+      </v-item-group>
+    </v-container>
+  </vcontainer>
+</template>
+<script>
+import axios from 'axios'
+export default {
+  asyncData({ params }) {
+    return axios(`https://barradois.com/wp-json/wp/v2/posts`)
+      .then((response) => {
+        return { posts: response.data }
+      })
+      .catch((error) => {
+        return { error: error }
+      })
+  },
+  data() {
+    return {
+      posts: {},
+      error: [],
+    }
+  },
+}
+</script>
