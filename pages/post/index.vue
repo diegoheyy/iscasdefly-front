9a9f3f3d0713710f26a93788e2690eff4b531f29
Diego Hey <diegoheyy@gmail.com>
2021-05-07 19:58:03 -0300

    fix dev

--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,22 +1,6 @@
 <template>
   <vcontainer>
-<<<<<<< HEAD
-    <v-btn color="primary" elevation="2" large>ALOW</v-btn>
-    <v-container class="pa-1">
-      <v-item-group>
-        <v-row>
-          <v-col v-for="(post, i) in posts" :key="i" cols="12" md="6">
-            <v-img :src="`${post.fimg_url}`" height="150"></v-img>
-            <v-item>
-              {{ post.title.rendered }}
-            </v-item>
-          </v-col>
-        </v-row>
-      </v-item-group>
-    </v-container>
-=======
     <theListPost :posts="posts"></theListPost>
->>>>>>> 8f38ac0cee78953a955f2fd7c08ad78e9f6a5c28
   </vcontainer>
 </template>
 <script>
@@ -25,11 +9,7 @@ import theListPost from '../../components/theListPost.vue'
 export default {
   components: { theListPost },
   asyncData({ params }) {
-<<<<<<< HEAD
-    return axios(`http://localhost/wordpress/wp-json/wp/v2/posts`)
-=======
     return axios(`https://iscasdefly.000webhostapp.com//wp-json/wp/v2/posts`)
->>>>>>> 8f38ac0cee78953a955f2fd7c08ad78e9f6a5c28
       .then((response) => {
         return { posts: response.data }
       })
