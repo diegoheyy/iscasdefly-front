67822c659224c3afda245337f2ce6cab6685e40a
Diego Hey <diegoheyy@gmail.com>
2021-06-01 10:07:32 -0300

    feat: add footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,12 +3,9 @@
     <v-main>
       <v-app-bar color="primary" app dark>
         <v-app-bar-nav-icon>
-           <v-btn icon nuxt href="/"><v-icon>mdi-home</v-icon></v-btn>
+          <v-btn icon nuxt href="/"><v-icon>mdi-home</v-icon></v-btn>
         </v-app-bar-nav-icon>
-        <v-toolbar-title>
-         Iscas de Fly
-        </v-toolbar-title
-        >
+        <v-toolbar-title > Iscas de Fly </v-toolbar-title>
         <v-spacer></v-spacer>
         <v-btn icon nuxt href="/post"> Blog </v-btn>
         <v-btn
@@ -23,69 +20,10 @@
         <nuxt />
       </v-container>
     </v-main>
-  </v-app>
-
-  <!-- <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar :clipped-left="clipped" fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
-    <v-main>
-      <v-container>
-        <nuxt />
-      </v-container>
-    </v-main>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light> mdi-repeat </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :absolute="!fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+    <v-footer absolute elevation="10" padless app color="primary">
+      <v-col class="white--text text-center py-1">Zero / Dez - Fry Fishing  &copy; {{ new Date().getFullYear() }}</v-col>
     </v-footer>
-  </v-app> -->
+  </v-app>
 </template>
 
 <script>
